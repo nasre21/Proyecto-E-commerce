6206e38904933112ac773df12731dbe324ed045d
nasre21 <nasou2001@gmail.com>
2023-02-07 10:29:17 +0100

    modificando link desde javascript

--- catalogo.js
+++ catalogo.js
@@ -3,7 +3,7 @@ const buTon = document.getElementById("imagen1");
 buTon.addEventListener("click",open);
 
 function open(){
-  let estado=buTon
+  let estado=buTon.getAttribute("src", "")
   if(buTon == ){
     document.location.assign("/descripcionproducto.html");
   }
