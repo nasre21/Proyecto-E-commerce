46140debf204cc62741363f4894bade0418f9ada
JJessa <jessicamosteiro13@gmail.com>
2023-02-09 10:40:21 +0100

    hecha función para el login y el formulario de registro

--- js/validacion.js
+++ js/validacion.js
@@ -105,29 +105,126 @@
 
 // });
 
-// formulario.addEventListener("submit", (e) => {
-//     e.preventDefault();
-// })
+// // formulario.addEventListener("submit", (e) => {
+// //     e.preventDefault();
+// // })
 
-const form1 = document.getElementById('container-login-two');
-const mail = document.getElementById('mail');
-const password = document.getElementById('password');
-const parrafo = document.getElementById('warnings');
+// const form1 = document.getElementById('container-login-two');
+// const mail = document.getElementById('mail');
+// const password = document.getElementById('password');
+// const parrafo = document.getElementById('warnings');
 
-boton.addEventListener('click', e=>{
-    e.preventDefault()
-});
+// boton.addEventListener('click', e=>{
+//     e.preventDefault()
+// });
 
-// function validacion(correo){
-    // let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+// // function validacion(correo){
+//     // let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
-    // let esValido = expReg.test(correo); 
+//     // let esValido = expReg.test(correo); 
 
-    if(esValido==true) {
-        alert("El email es válido");
+//     if(esValido==true) {
+//         alert("El email es válido");
+
+//     }
+//     else{
+//         alert("El email no es válido");
+
+//     }
+
+function saveUserData(usuario, contraseña, nombre, apellido, nacimiento, correo, direccion, codigoPostal, provincia, numero, identidad, portal, piso) {
+    const user = {
+        usuario: "pepe",
+        contraseña: "123445",
+        nombre: "Juanka",
+        apellido: "Martinez",
+        nacimiento: "16/04/1999",
+        correo: "usuario@usuario.com",
+        direccion: "Bolueta",
+        portal: "9",
+        piso: "3",
+        codigoPostal: "48004",
+        provincia: "Bizkaia",
+        numero: "612121213",
+        identidad: "78564521L",
+        tipoUsuario: "administrador"
+    };
+    // const cliente = {
+    //     usuario: "pepe",
+    //     contraseña: "123445",
+    //     nombre: "Juanka",
+    //     apellido: "Martinez",
+    //     nacimiento: "16/04/1999",
+    //     correo: "usuario@usuario.com",
+    //     direccion: "Bolueta",
+    //     portal: "9",
+    //     piso: "3",
+    //     codigoPostal: "48004",
+    //     provincia: "Bizkaia",
+    //     numero: "612121213",
+    //     identidad: "78564521L",
+    //     tipoUsuario: "cliente"
+    // };
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+
+  function getUserData() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+
+  function handleRegistration(event) {
+    event.preventDefault();
+
+    const usuario = document.getElementById("usuario").value;
+    const contrasenna = document.getElementById("contraseña").value;
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellidos").value;
+    const nacimiento = document.getElementById("edad").value;
+    const correo = document.getElementById("correo-registro").value;
+    const direccion = document.getElementById("direccion").value;
+    const codigoPostal = document.getElementById("codigopostal").value;
+    const provincia = document.getElementById("provincias").value;
+    const numero = document.getElementById("telefono").value;
+    const identidad = document.getElementById("identificacion").value;
+    const portal = document.getElementById("portal").value;
+    const piso = document.getElementById("piso").value;
+
+
+    // const userType = document.getElementById("user-type").value;
+
+    saveUserData(usuario, contrasenna, nombre, apellido, nacimiento, correo, direccion, codigoPostal, provincia, numero, identidad, portal, piso);
+
+    alert("Registro exitoso. Por favor inicie sesión.");
+  
+    window.location.href = "login-page.html";
 
     }
-    else{
-        alert("El email no es válido");
 
-    }
+    function handleLogin(event) {
+        event.preventDefault();
+      
+        const user = getUserData();
+        if (!user) {
+          alert("No hay ningún usuario registrado. Por favor regístrese.");
+          return;
+        }
+      
+        const mail = document.getElementById("email").value;
+        const password = document.getElementById("pasword").value;
+      
+        if (mail === user.correo && password === user.contrasenna) {
+          if (user.tipoUsuario === "administrador") {
+            window.location.href = "index.html";
+          } else {
+            window.location.href = "cliente";
+          }
+        } else {
+          alert("Nombre de usuario o contraseña incorrectos.");
+        }
+      }
+      
+      const registrationForm = document.getElementById("formulario");
+      registrationForm.addEventListener("submit", handleRegistration);
+
+      const loginForm = document.getElementById("btn");
+      loginForm.addEventListener("click", handleLogin);
