d9e323cccff776742243a58e3a779256f58a7514
JJessa <jessicamosteiro13@gmail.com>
2023-02-10 10:14:12 +0100

    añadir intranet.html y validacion de pagina de login

--- js/validacion.js
+++ js/validacion.js
@@ -68,31 +68,121 @@ function saveUserData(usuario, contraseña, nombre, apellido, nacimiento, correo
 
     }
 
-    function handleLogin(event) {
-        event.preventDefault();
+    // function handleLogin(event) {
+    //     event.preventDefault();
       
-        const user = getUserData();
-        if (!user) {
-          alert("No hay ningún usuario registrado. Por favor regístrese.");
-          return;
-        }
+    //     const user = getUserData();
+    //     if (!user) {
+    //       alert("No hay ningún usuario registrado. Por favor regístrese.");
+    //       return;
+    //     }
       
-        const usuario = document.getElementById("email").value;
-        const password = document.getElementById("pasword").value;
+    //     const usuario = document.getElementById("email").value;
+    //     const password = document.getElementById("pasword").value;
       
-        if (usuario === user.usuario && password === user.contraseña) {
-          if (user.tipoUsuario === "administrador") {
-            window.location.href = "index.html";
-          } else {
-            window.location.href = "cliente.html";
-          }
-        } else {
-          alert("Nombre de usuario o contraseña incorrectos.");
+    //     if (usuario === user.usuario && password === user.contraseña) {
+    //       if (user.tipoUsuario === "administrador") {
+    //         window.location.href = "index.html";
+    //       } else {
+    //         window.location.href = "cliente.html";
+    //       }
+    //     } else {
+    //       alert("Nombre de usuario o contraseña incorrectos.");
+    //     }
+    //   }
+      
+    //   const registrationForm = document.getElementById("formulario");
+    //   registrationForm.addEventListener("submit", handleRegistration);
+
+    //   const loginForm = document.getElementById("btn");
+    //   loginForm.addEventListener("submit", handleLogin);
+
+    function obtenerListaUsuarios(){
+      let listaUsuarios = JSON.parse(localStorage.getItem('user1'));
+
+
+      if(listaUsuarios == null) {
+        listaUsuarios =
+        [
+        ["pepe","123445","Juanka","Martinez", "16/04/1999", "usuario@usuario.com","Bolueta","9","3","48004","Bizkaia","612121213","78564521L", "administrador"],
+        ["JM","12345","Jose","Perez", "08/11/1996", "info@usuario.com","Bolueta","9","3","48004","Bizkaia","612121213","78564521L", "cliente"]
+      ]
+
+
+      }
+      return listaUsuarios;
+    }
+    function validarCredenciales(pCorreo , pContrasenna) {
+      let listaUsuarios = obtenerListaUsuarios();
+      let bAcceso = false;
+
+
+      for(let i = 0; i< listaUsuarios.length; i++) {
+        if(pCorreo == listaUsuarios[i][5] && pContrasenna == listaUsuarios[i][1]) {
+          bAcceso = true;
+          sessionStorage.setItem('usuarioActivo', listaUsuarios[i][2] + ' ' + listaUsuarios[i][3]);
+          sessionStorage.setItem('rolUsuarioActivo', listaUsuarios[i][13]);
         }
       }
-      
-      const registrationForm = document.getElementById("formulario");
-      registrationForm.addEventListener("submit", handleRegistration);
 
-      const loginForm = document.getElementById("btn");
-      loginForm.addEventListener("submit", handleLogin);
+      
+      // listaUsuarios.forEach(elemento => {
+      //      if(pCorreo == indexOf(elemento[5]) && pContrasenna == elemento[1]) {
+      //     bAcceso = true;
+      //      sessionStorage.setItem('usuarioActivo', listaUsuarios[elemento][2] + ' ' + listaUsuarios[elemento][3]);
+      //      sessionStorage.setItem('rolUsuarioActivo', listaUsuarios[elemento][13]);
+      //   }
+
+        
+      // });
+
+
+      return bAcceso;
+    }
+
+
+
+
+    document.querySelector('#btn').addEventListener('click', iniciarSesion);
+
+
+
+
+    function iniciarSesion(){
+      var sCorreo = '';
+      var sContrasenna = '';
+      var bAcceso = false;
+
+
+      sCorreo = document.querySelector('#email').value;
+      sContrasenna = document.querySelector('#pasword').value;
+
+
+      bAcceso = validarCredenciales(sCorreo, sContrasenna);
+     
+     
+      if(bAcceso == true) {
+        ingresar();
+      }
+
+
+    }
+
+
+
+
+    function ingresar() {
+      var rol = sessionStorage.getItem('rolUsuarioActivo');
+      switch(rol){
+        case 'administrador':
+          window.location.href = 'intranet.html';
+          break;
+          case 'cliente':
+            window.location.href = 'cliente.html';
+            break;
+            default:
+
+
+            break;
+      }
+    }
