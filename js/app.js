4b1bff67689942b96355cc722a7c69fca77c12bf
Anismelow <117463292+Anismelow@users.noreply.github.com>
2023-02-11 16:53:02 +0100

    arreglo de la fecha

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
+// mostrar fecha en pantalla 
+function showDate() {
+  const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+  const date = new Date();
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  document.getElementById("fecha").innerHTML = `${day} de ${month} del ${year}`;
+}
 
-let fecha = document.getElementById("fecha");
-
-let dataFecha = new Date();
-
-fecha.innerHTML = dataFecha.toLocaleDateString()
-
-
-
-
-
-
-
-
-
-
-
+showDate();
 
 
 // suma y resta productos del carrito
