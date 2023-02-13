b83e29b7b3ee28e0abf95f2669b14c9ce3f345c2
Tu Nombre <nasou2001@gmail.com>
2023-02-13 22:21:55 +0100

    hacer una prueba de cambiar idioma

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,98 +1,126 @@
 // link Catalogo
-const buTon = document.getElementById("imagen1");
-const buTon2 = document.getElementById("imagen2");
-const buTon3 = document.getElementById("imagen3");
-const buTon4 = document.getElementById("imagen4");
-const buTon5 = document.getElementById("imagen5");
-const buTon6 = document.getElementById("imagen6");
-const buTon7 = document.getElementById("imagen7");
-const buTon8 = document.getElementById("imagen8");
-const buTon9 = document.getElementById("imagen9");
-const buTon10 = document.getElementById("imagen10");
-const buTon11 = document.getElementById("imagen11");
-const buTon12 = document.getElementById("imagen12");
+// const buTon = document.getElementById("imagen1");
+// const buTon2 = document.getElementById("imagen2");
+// const buTon3 = document.getElementById("imagen3");
+// const buTon4 = document.getElementById("imagen4");
+// const buTon5 = document.getElementById("imagen5");
+// const buTon6 = document.getElementById("imagen6");
+// const buTon7 = document.getElementById("imagen7");
+// const buTon8 = document.getElementById("imagen8");
+// const buTon9 = document.getElementById("imagen9");
+// const buTon10 = document.getElementById("imagen10");
+// const buTon11 = document.getElementById("imagen11");
+// const buTon12 = document.getElementById("imagen12");
 
 
-const pruducto = document.getElementById("product");
-const catalogo1 = document.getElementById("catalogo1");
-
-
-
+// const pruducto = document.getElementById("product");
+// const catalogo1 = document.getElementById("catalogo1");
 
 
 
 
 
 
-  buTon.addEventListener("click",open1);
-  buTon2.addEventListener("click",open2);
-  buTon3.addEventListener("click",open3);
-  buTon4.addEventListener("click",open4);
-  buTon5.addEventListener("click",open5);
-  buTon6.addEventListener("click",open6);
-  buTon7.addEventListener("click",open7);
-  buTon8.addEventListener("click",open8);
-  buTon9.addEventListener("click",open9);
-  buTon10.addEventListener("click",open10);
-  buTon11.addEventListener("click",open11);
-  buTon12.addEventListener("click",open12);
+
+
+
+//   buTon.addEventListener("click",open1);
+//   buTon2.addEventListener("click",open2);
+//   buTon3.addEventListener("click",open3);
+//   buTon4.addEventListener("click",open4);
+//   buTon5.addEventListener("click",open5);
+//   buTon6.addEventListener("click",open6);
+//   buTon7.addEventListener("click",open7);
+//   buTon8.addEventListener("click",open8);
+//   buTon9.addEventListener("click",open9);
+//   buTon10.addEventListener("click",open10);
+//   buTon11.addEventListener("click",open11);
+//   buTon12.addEventListener("click",open12);
 
 
 
 
 
 
-function open1(){
-   window.location.assign("/descripcionproducto.html");
+// function open1(){
+//    window.location.assign("/descripcionproducto.html");
 
-   catalogo1.repla
+//    catalogo1.repla
 
-   }
+//    }
   
 
-function open2(){
-  window.location.assign("/descripcionproducto.html");
-  }
+// function open2(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
   
-function open3(){
-  window.location.assign("/descripcionproducto.html");
-  }
+// function open3(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
   
-function open4(){
-  window.location.assign("/descripcionproducto.html");
-  }
+// function open4(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
   
-function open5(){
-  window.location.assign("/descripcionproducto.html");
-  }
+// function open5(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
   
-function open6(){
-  window.location.assign("/descripcionproducto.html");
-  }
+// function open6(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
   
-function open7(){
-  window.location.assign("/descripcionproducto.html");
+// function open7(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
+
+// function open8(){
+//   window.location.assign("/descripcionproducto.html");
+//     }
+
+// function open9(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
+
+// function open10(){
+//    window.location.assign("/descripcionproducto.html");
+//   }
+
+// function open11(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
+
+// function open12(){
+//   window.location.assign("/descripcionproducto.html");
+//   }
+
+   catalogo
+//cambio de idiomas
+const cambioIdioma = get 
+
+  let days =[
+    //Rascadores
+    {
+        id: "rascador-01",
+        nombre: "Cueva Trixie con bola rascadora",
+        descripcion: "Elegante cueva para gatos, gran bola rascadora de yute, mucha superficie para afilar las uñas, amplio escondite, forrada con cálido vellón y felpa suave texturizada, medidas: 36 x 50 cm (Diám x Al)",
+        imagen: "../image/gatos/rascadores/rascador01_trixie_hoehle_kratzball.jpg",
+        categoria: {
+            nombre: "Gatos",
+            id: "gato"
+        },
+        precio: 60.99
+    },
+
+    {
+      id: "rascador-01",
+      nombre: "Cueva Trixie con bola",
+      descripcion: "Elegante cueva para gatos, gran bola rascadora de yute, mucha superficie para afilar las uñas, amplio escondite, forrada con cálido vellón y felpa suave texturizada, medidas: 36 x 50 cm (Diám x Al)",
+      imagen: "../image/gatos/rascadores/rascador01_trixie_hoehle_kratzball.jpg",
+      categoria: {
+          nombre: "Gatos",
+          id: "gato"
+      },
+      precio: 60.99
   }
-
-function open8(){
-  window.location.assign("/descripcionproducto.html");
-    }
-
-function open9(){
-  window.location.assign("/descripcionproducto.html");
-  }
-
-function open10(){
-   window.location.assign("/descripcionproducto.html");
-  }
-
-function open11(){
-  window.location.assign("/descripcionproducto.html");
-  }
-
-function open12(){
-  window.location.assign("/descripcionproducto.html");
-  }
-
-  // catalogo
-
+  ]
