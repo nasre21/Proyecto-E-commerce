fea42a793a4d68d2ab53657a4fe4498c49c61534
trontzal <trontzal@gmail.com>
2023-02-13 11:35:32 +0100

    ontento de pull

--- js/cambioIdioma.js
+++ js/cambioIdioma.js
@@ -5,12 +5,10 @@ const textsToChange = document.querySelectorAll("[data-section]");
 const changeLanguage = async (language) => {
     const requestJson = await fetch(`../idiomas/${language}.json`);
     const texts = await requestJson.json();
-     console.log(texts);
-     
+
     for(textToChange of textsToChange) {
         const section = textToChange.dataset.section;
         const value = textToChange.dataset.value;
-        
         console.log(section, value);
     }
 };
