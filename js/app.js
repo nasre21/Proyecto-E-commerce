7c82cda6f6de35830c3b8cf88a10468c38e98e9e
Amaia <amaia.berrocal@gmail.com>
2023-02-14 10:22:38 +0100

    muevo cambiar idioma a app.js y elimino archivo que sobra

--- js/app.js
+++ js/app.js
@@ -10,6 +10,28 @@ function showDate() {
 
 showDate();
 
+//cambiar idioma
+const flagsElement = document.getElementById('flags');
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`../idiomas/${language}.json`);
+    const texts = await requestJson.json();
+     console.log(texts);
+     
+    for(const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        
+        textToChange.innerHTML = texts[section][value];
+    }
+};
+
+flagsElement.addEventListener('click', (e) => {
+    changeLanguage(e.target.value);
+});
+
 
 
 
