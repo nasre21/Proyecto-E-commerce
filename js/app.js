a450265030dd4a21532a6136bfd9e482d84fba10
nasre21 <nasou2001@gmail.com>
2023-02-15 11:58:41 +0100

    cambios de idioma

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-// mostrar fecha en pantalla 
-function showDate() {
-  const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-  const date = new Date();
-  const day = date.getDate();
-  const month = months[date.getMonth()];
-  const year = date.getFullYear();
-  document.getElementById("fecha").innerHTML = `${day} de ${month} del ${year}`;
-}
-
-showDate();
 
 //cambiar idioma
 const flagsElement = document.getElementById('flags');
