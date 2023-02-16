68b6a86a0d50da67b4080fcdd4642c7dbbf9bd3d
Amaia <amaia.berrocal@gmail.com>
2023-02-16 08:42:25 +0100

    cambios desechables

--- js/user_amaia.js
+++ js/user_amaia.js
@@ -1,47 +1,43 @@
+//registrar usuario
 const inputusuario = document.querySelector("#usuario");
 const inputnombre = document.querySelector("#nombre");
 const inputapellidos = document.querySelector("#apellidos");
-// const inputedad = document.querySelector("#edad").value;
-// const inputcorreo = document.querySelector("#correo-registro").value;
-// const inputpassword = document.querySelector("#password").value;
-// const inputcalle = document.querySelector("#calle").value;
-// const inputportal = document.querySelector("#portal").value;
-// const inputpiso = document.querySelector("#piso").value;
-// const inputcodigoPostal = document.querySelector("#codigopostal").value;
-// const inputprovincia = document.querySelector("#provincia").value;
-// const inputtelefono = document.querySelector("#telefono").value;
-// const inputidentificacion = document.querySelector("#identificacion").value;
-const botonEnviar = document.querySelector("#registro");
+const inputedad = document.querySelector("#edad");
+const inputcorreo = document.querySelector("#correo-registro");
+const inputpassword = document.querySelector("#password");
+const inputcalle = document.querySelector("#calle");
+const inputportal = document.querySelector("#portal");
+const inputpiso = document.querySelector("#piso");
+const inputcodigoPostal = document.querySelector("#codigopostal");
+const inputprovincia = document.querySelector("#provincia");
+const inputtelefono = document.querySelector("#telefono");
+const inputidentificacion = document.querySelector("#identificacion");
+
+const form = document.getElementById("formulario");
 
 const usuarios = [];
 
-botonEnviar.addEventListener("click", guardarUsuario);
+form.addEventListener("submit", guardarUsuario);
 
 function guardarUsuario(event) {
     event.preventDefault();
-
-    usuario= inputusuario.value;
-    nombre= inputnombre.value;
-    apellidos= inputapellidos.value;
-    // inputedad: edad,
-    // inputcorreo: correo,
-    // inputpassword: password,
-    // inputcalle: calle,
-    // inputportal: portal,
-    // inputpiso: piso,
-    // inputcodigoPostal: codigoPostal,
-    // inputprovincia: provincia,
-    // inputtelefono: telefono,
-    // inputidentificacion: identificacion
+      
     let user = {
         usuario: inputusuario.value,
         nombre: inputnombre.value,
         apellidos: inputapellidos.value,
+        edad: inputedad.value,
+        correo: inputcorreo.value,
+        password: inputpassword.value,
+        calle: inputcalle.value,
+        portal: inputportal.value,
+        piso: inputpiso.value,
+        codigoPostal: inputcodigoPostal.value,
+        provincia: inputprovincia.value,
+        telefono: inputtelefono.value,
+        identificacion: inputidentificacion.value
     }
     usuarios.push(user);
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
     console.log(usuarios);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));    
 }
-
-
-
