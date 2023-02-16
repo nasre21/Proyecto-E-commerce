25ef8e7ae15d75f91a8c5afac674db77d91d7638
trontzal <trontzal@gmail.com>
2023-02-16 10:33:55 +0100

    descuento de iva

--- js/carrito.js
+++ js/carrito.js
@@ -17,10 +17,22 @@ function changeValue(id, operation) {
 //  esto hace la suma del carrito
   const price1 = parseFloat(document.getElementById("precioSinDescuento").innerHTML);
   const price2 = parseFloat(document.getElementById("envio").innerHTML);
-  const discount = parseFloat(document.getElementById("descuento").innerHTML)/100;
-  
-  const total = (price1 + price2) * (1 - discount);
-  document.getElementById("total").innerHTML = total;
+  const totalConIva = (price1 + price2);
+  document.getElementById("total").innerHTML = totalConIva;
+
+  //aplicar el descuento de iva
+  const descuento = document.getElementById("descuento");
+  descuento.addEventListener("change", () => {
+    if(descuento.checked){
+      const quitarIva = totalConIva * (100 - 21)/100;
+      document.getElementById("total").innerHTML = quitarIva;
+    }else{
+      document.getElementById("total").innerHTML = totalConIva;
+    }
+  })
+
+
+
 
 
 //agregar al carrito
