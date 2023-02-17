06e4755fb255e7d59b0124645133638021da3966
JJessa <jessicamosteiro13@gmail.com>
2023-02-17 14:02:12 +0100

    resolucion conflicto funcion fecha

--- js/app.js
+++ js/app.js
@@ -42,17 +42,6 @@ window.addEventListener('mouseout', function(event) {
   });
 
 
-// mostrar fecha en pantalla 
-function showDate() {
-  const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-  const date = new Date();
-  const day = date.getDate();
-  const month = months[date.getMonth()];
-  const year = date.getFullYear();
-  document.getElementById("fecha").innerHTML = `${day} de ${month} del ${year}`;
-}
-
-showDate();
   //Funcion para mostrar fecha
   function showDate() {
     const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
