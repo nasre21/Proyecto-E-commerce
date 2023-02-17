d95faff7df9408a1cfbe4309c72bb3eca90181a7
trontzal <trontzal@gmail.com>
2023-02-17 14:17:00 +0100

    carrito hace la suma faltan cosas

--- js/carrito.js
+++ js/carrito.js
@@ -1,35 +1,9 @@
-// suma y resta productos del carrito
-function changeValue(id, operation) {
-  var value
-    = parseInt(document.getElementById(id).value, 10);
-  value = isNaN(value) ? 0 : value;
-  if (operation === "increase") {
-    value++;
-  } else if (operation === "decrease") {
-    value = (value > 0) ? value - 1 : 0;
-  }
-  document.getElementById(id).value = value;
-}
 
 
 
 
-//  esto hace la suma del carrito
-const price1 = parseFloat(document.getElementById("precioSinDescuento").innerHTML);
-const price2 = parseFloat(document.getElementById("envio").innerHTML);
-const totalConIva = (price1 + price2);
-document.getElementById("total").innerHTML = totalConIva;
 
-//aplicar el descuento de iva
-const descuento = document.getElementById("descuento");
-descuento.addEventListener("change", () => {
-  if(descuento.checked){
-    const quitarIva = totalConIva * (100 - 21)/100;
-    document.getElementById("total").innerHTML = quitarIva;
-  }else{
-    document.getElementById("total").innerHTML = totalConIva;
-  }
-})
+
 
 
 
@@ -83,13 +57,30 @@ if (productosEnCarrito) {
    </div>
      `
      carritoVacio.appendChild(div);
-
-     console.log(precioporcantidad)
+     totalPrecio += precioporcantidad
+     
    });
    
-   
+   console.log(totalPrecio)
+   document.getElementById("precioSinDescuento").innerHTML = totalPrecio;
  }
 
 
 
 
+//  esto hace la suma del carrito
+const price1 = parseInt(totalPrecio)
+const price2 = parseInt(document.getElementById("envio").innerHTML);
+const totalConIva = (price1 + price2);
+document.getElementById("total").innerHTML = totalConIva;
+
+//aplicar el descuento de iva
+const descuento = document.getElementById("descuento");
+descuento.addEventListener("change", () => {
+  if(descuento.checked){
+    const quitarIva = totalConIva * (100 - 21)/100;
+    document.getElementById("total").innerHTML = quitarIva;
+  }else{
+    document.getElementById("total").innerHTML = totalConIva;
+  }
+})
