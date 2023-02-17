ae412e29cb27ad33f196d0923e2e73810e61416d
trontzal <trontzal@gmail.com>
2023-02-17 13:01:30 +0100

    carrito multplica la cantidad y el precio pero faltan cosas

--- js/carrito.js
+++ js/carrito.js
@@ -43,14 +43,20 @@ const carritoProductos = document.querySelector("#carrito-Productos");
 const carritoAcciones = document.querySelector("#carritoAcciones");
 const btnVaciar = document.querySelector("#carrito-accion-vaciar");
 let  caja1 = document.querySelector(".caja1");
+let totalPrecio = 0
+let precioporcantidad = 0
 
 console.log(productosEnCarrito);
 
 if (productosEnCarrito) {
 
-console.log(productosEnCarrito);
+
 
    productosEnCarrito.forEach(producto => {
+
+      precioporcantidad = producto.precio * producto.cantidad
+      console.log(precioporcantidad)
+
      const div = document.createElement("div");
      div.classList.add("carrito-productos", "tarjeta", "d-flex", "mb-2");
      div.innerHTML = 
@@ -62,24 +68,27 @@ console.log(productosEnCarrito);
          <div class="d-flex">
            <p data-section="carrito" data-value="cantidad">Cantidad:</p>
            <div class="d-flex justify-content-end w-100 gap-1">
-             <button class="btn-mas border rounded " onclick="changeValue('number1', 'decrease')">-</button>
-             <input class="number inputNumero rounded" type="text" id="number1" value="0">
-             <button class=" btn-menos border rounded" onclick="changeValue('number1', 'increase')">+</button>
+           <input type="number" min="0" name="contadorcarrito" class=" contadorcarrito w-25 text-center" value="${producto.cantidad}">
            </div>
          </div>
        </div>
        <div class="d-flex border-bottom">
          <p data-section="carrito" data-value="precio">Precio:</p>
-         <p class="d-flex justify-content-end w-100">${producto.precio}€</p>
+         <p class="d-flex justify-content-end w-100 precioIndividual">${producto.precio}€</p>
+       </div>
+       <div class="d-flex border-bottom">
+         <p data-section="carrito" data-value="precio">Precio Total:</p>
+         <p class="d-flex justify-content-end w-100 precioIndividual">€</p>
        </div>
      </div>
    </div>
      `
      carritoVacio.appendChild(div);
     
-    
+     
    });
-
+   
+   
  }
   
 
